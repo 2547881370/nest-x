6713d9d9ce3ba77d397c9672014e52b4bc0947dc
xuhao <2547881370@qq.com>
2021-04-01 17:13:58 +0800

    修改文件名 - x

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import { TasksModule } from './tasks/tasks/tasks.module';
+import { TasksModule } from './tasks/x/tasks.module';
 import { ConfigModule, ConfigService } from 'nestjs-config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { resolve } from 'path';
